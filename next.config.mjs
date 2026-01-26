c3b70023db87accb2e688640306134d9b0eac625
TimaJunior <litvatimati@gmail.com>
2026-01-26 16:59:32 +0200

    feat: Set Next.js server actions body size limit to 50MB.

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
+  },
 };
 
 export default nextConfig;
